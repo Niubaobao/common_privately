6c533db6716db5692e7e76d4bbeb9de9b8cf44bc
Niubaobao <451142214@qq.com>
2021-07-08 19:08:45 +0800

    docs(规范提交message): 统一处理git message

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# 0.1.0 (2021-07-08)
+
+范围|描述|commitId
+--|--|--
+ - | test makefile & test npm | [9c0abbd](https://github.com/Niubaobao/common_privately/commit/9c0abbd)
+ - | first init | [0b313b1](https://github.com/Niubaobao/common_privately/commit/0b313b1)
+
---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2021-07-08 19:04:19
+ * @LastEditTime: 2021-07-08 19:04:30
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /utils/commitlint.config.js
+ */
+
+module.exports = {
+    extends: ['./node_modules/vue-cli-plugin-commitlint/lib/lint']
+}  
